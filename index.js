f2dd8c18af571acc54eea01ed093e8a98fff324d
access6080 <access.6080@gmail.com>
2021-12-25 12:03:03 -0500

    Ready for Heroku

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import express from 'express';
 import mongoose from 'mongoose';
 import  cors from 'cors';
 import dotenv from 'dotenv';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 dotenv.config(); //Initialize enviroment variables
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 //Route Imports
 import apiRoute from './routes/api.js';
@@ -11,6 +16,8 @@ import bookRoutes from './routes/books.js'
 // Initialize Server
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static(join(__dirname, 'client', 'build')));
 
 // Initialize Database
 mongoose.connect(process.env.CONNECTION_URI, {
@@ -31,6 +38,10 @@ app.use(cors({
 //Linking Custom Routes
 app.use('/api', apiRoute);
 app.use('/book', bookRoutes);
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'client', 'build', 'index.html'));
+});
 
 
 app.listen(PORT, () => {
