f72bb75c1d5b5243bc8066e5fdd5da3352c3cf69
access6080 <access.6080@gmail.com>
2021-12-27 01:57:51 -0500

    Fixed slidespeerview for small screens

--- client/src/components/BookListContainer.jsx
+++ client/src/components/BookListContainer.jsx
@@ -8,10 +8,10 @@ const BookListContainer = () => {
     return (
         <div className="pt-4 w-screen h-2/3 ">
             <CategoryList />
-            <BookList category="Science Fiction"/>  
-            <BookList category="Mystery"/>  
-            <BookList category="Fantasy"/>  
-            <BookList category="Action" />  
+            <BookList  category="Science Fiction"/>  
+            <BookList  category="Mystery"/>  
+            <BookList  category="Fantasy"/>  
+            <BookList  category="Action" />  
             <Footer /> {/*TODO: Complete Footer */}
         </div>
     )
